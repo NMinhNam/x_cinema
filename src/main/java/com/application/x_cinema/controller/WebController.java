2038e6c45c7c663a0e31b010f092392643d054e1
NMinhNam <minhnam1810@gmail.com>
2025-04-29 09:38:50 +0700

    feat(HomeUI): init form login

---
+++ src/main/java/com/application/x_cinema/controller/WebController.java
@@ -0,0 +1,18 @@
+package com.application.x_cinema.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index"; // Trả về file index.html
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login"; // Trả về file login.html
+    }
+}
