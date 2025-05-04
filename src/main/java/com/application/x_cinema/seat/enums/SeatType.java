09763c1c32e2a4e78e9dba7fead1d685e98cc98e
C0ngHung <C0ngHungDCH@gmail.com>
2025-05-04 23:11:00 +0700

    Commit07:CongHung:fix_enum_SeatStatus_and_enum_SeatType

--- src/main/java/com/application/x_cinema/seat/enums/SeatType.java
+++ src/main/java/com/application/x_cinema/seat/enums/SeatType.java
@@ -1,14 +1,34 @@
 package com.application.x_cinema.seat.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public enum SeatType {
-    STANDARD("Standard"),
+    STANDARD("standard"),
     VIP("VIP"),
-    COUPLE("Couple");
+    LUXURY("luxury"),
+    BEAN_BAG("bean bag"),
+    ROCKING_CHAIR("rocking chair");
 
-    private final String displayName;
+    private final String value;
+
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+
+    @JsonCreator
+    public static SeatType fromValue(String value) {
+        for (SeatType type : values()) {
+            if (type.value.equalsIgnoreCase(value)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Invalid seat type: " + value);
+    }
 }
